ecbdb193c3a0f022a77f708e9004b5543d0d5345
David <d@d.c>
2022-09-25 23:00:19 +0200

    Add automatically generating color pickers

---
+++ arduino_scripts/scripts/esp_script/data/web/res/js/generate_color_pickers.js
@@ -0,0 +1,12 @@
+
+const colorPickerHtml = '<div class="color-wrapper"> <input type="color" id="color-ID" name="color-ID" class="color-container-picker" value="#DEDEDE"><br> <img src="../res/img/light_bulb.png" class="color-container-design"></div>';
+
+function generateColorPickers(amount, accuracy)
+{
+    for(var i = 0; i < amount / accuracy; i++)
+    {
+        document.getElementById("color-container").innerHTML += colorPickerHtml.replaceAll('ID', '' + (i + 1));
+    }
+}
+
+generateColorPickers(10, 2);
